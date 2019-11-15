434884afef867bcf41d24cf95751e135e8668e03
Krit Verma <kritverma27@gmail.com>
2019-11-15 21:25:52 +0530

    added drop probability

--- client.py
+++ client.py
@@ -23,47 +23,74 @@ s.connect((host, port))
 window=[]
 N = 7
 Sn=seqNo(0)
-Sb=0
+Sb=seqNo(0)
+
+first_frame_check=True
+read_count=1
+frame_sent_count=0
 
 #SENDS DATA
 fileOpen= open('data.txt', 'rb') 
 data = fileOpen.read(50)
 done = False
+
 lastackreceived = time.time()
+starttime = time.time()
 
 while not done:
 	if (Check_Window_Size) :
 		# Create a new packet packet(sq_no,data,check_sum)
-		pkt={
-		'sq_no' : Sn ,
-		'data' : data,
-		}
+		if(Sb==Sn):
+			pkt={
+			'sq_no' : Sn ,
+			'data' : data,
+			}
 
-		# Convert The Packet Into Byte Format
-		byte_pkt=pickle.dumps(pkt)
-		
-		# Create Checksum 
-		check_sum=hashlib.sha224(byte_pkt).hexdigest()
-		
-		# Add Checksum to Pakect
-		pkt['check_sum']=check_sum
-		
-		# Send Packet
-		s.sendall(pickle.dumps(pkt))
-		print('Sent Data', repr(Sn))
+			# Convert The Packet Into Byte Format
+			byte_pkt=pickle.dumps(pkt)
+			
+			# Create Checksum 
+			check_sum=hashlib.sha224(byte_pkt).hexdigest()
+			
+			# Add Checksum to Pakect
+			pkt['check_sum']=check_sum
+			
+			# Send Packet
+			s.sendall(pickle.dumps(pkt))
+			print('Sent Packet', repr(Sn))
+			
+			if first_frame_check:
+				first_frame_time=time.time()
+				first_frame_check=False
 
-		# Increment Sn for sending next packet
-		Sn=seqNo(Sn+1)
+			frame_sent_count=frame_sent_count+1
 
-		#check if EOF has reached
-		if(not data):
-			done = True
-		
-		#append packet to window
-		window.append(pkt)
+			# Increment Sn for sending next packet
+			Sn=seqNo(Sn+1)
 
-		#read more data
-		data = fileOpen.read(50)
+			#check if EOF has reached
+			if(not data):
+				done = True
+			
+			#append packet to window
+			window.append(pkt)
+
+			#read more data
+			data = fileOpen.read(50)
+			read_count=read_count+1
+		else:
+
+			s.sendall(pickle.dumps(window[0]))
+			
+			if first_frame_check:
+				first_frame_time=time.time()
+				first_frame_check=False
+			
+			del window[0]
+			print('Sent Packet After Drop', repr(Sb))
+			Sb = seqNo(Sb + 1)
+			frame_sent_count=frame_sent_count+1
+
 
 	try:
 		pkt = s.recv(4096)
@@ -78,21 +105,38 @@ while not done:
 		if(calc_check_sum!=recv_check_sum):
 			print("Error in Ack")
 		else:
-			print('Received Ack for', repr(rcv_pkt['sq_no']))
-			while rcv_pkt['sq_no']>Sb and window:
+			print('Received Ack for Packet', repr(rcv_pkt['sq_no']))
+			if rcv_pkt['sq_no']>Sb or (rcv_pkt['sq_no']==0 and Sb==7):
 				lastackreceived = time.time()
 				del window[0]
-				Sb = Sb + 1
-	
+				Sb = seqNo(Sb + 1)
+			
+				
 	except Exception as e:
 		curr_time=time.time()
 		transmisiom_time=curr_time-lastackreceived
 		if(transmisiom_time>0.01):
 			for i in window:
-				s.sendall(i)
+				s.sendall(pickle.dumps(i))
+
+endtime = time.time()
 
 fileOpen.close()  
 s.close()
+
+print ('FILE TRANFER SUCCESSFUL')
 print("Client Disconnected")
+
+print("--------------------------")
+
+print("Start Time", starttime)
+print("First Frame Sent at",first_frame_time)
+print("End Time", endtime)
+
+avg_frame_sent=frame_sent_count/read_count
+print("Average Number of Frame Sent",avg_frame_sent)
+
+print("--------------------------")
+
 
 
--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def seqNo(x):
 	return x%8
 
 def Generate_Probability():
-	x=random.uniform(0.4,0.7)
+	x=random.uniform(0.45,0.7)
 	return round(x,2)
 
 host = ''
@@ -36,13 +36,17 @@ ACK = 1
 acks = []
 
 Acc_Prob = 0.5
+if_dropped=0
+first_frame_check=True
 
 f = open("output.txt", "wb")
 endoffile = False
+
 lastpktreceived = time.time()	
 starttime = time.time()
 
 while True:
+	p=Generate_Probability()
 	try:
 		pkt = conn.recv(4096)
 		rcv_pkt=pickle.loads(pkt)
@@ -52,66 +56,80 @@ while True:
 		byte_rcv_pkt=pickle.dumps(rcv_pkt)
 		calc_check_sum=hashlib.sha224(byte_rcv_pkt).hexdigest()
 		
-		p=Generate_Probability()
-		print(p)
-	
-		if(calc_check_sum!=recv_check_sum):
-			print("Error in Packet")
-		else:
-			if(rcv_pkt['sq_no']!=Exp_Sn):
-				print ("Received out of order", rcv_pkt['sq_no'])
-				ack_pkt={
-				'sq_no': Exp_Sn
-				}
-				# Convert The Packet Into Byte Format
-				byte_pkt=pickle.dumps(ack_pkt)
+		if (p>Acc_Prob or if_dropped==1):
+			if(calc_check_sum!=recv_check_sum):
+				print("Error in Packet")
 
-				# Create Checksum 
-				check_sum=hashlib.sha224(byte_pkt).hexdigest()
-
-				# Add Checksum to Pakect
-				ack_pkt['check_sum']=check_sum
-
-				# Send Packet
-				conn.sendall(pickle.dumps(ack_pkt))
-				print('Ack Of Same Packet', repr(Exp_Sn))
 			else:
-				print ("Received inorder", Exp_Sn)
-				
-				if rcv_pkt['data']:
-					f.write(rcv_pkt['data'])
-				else:
-					endoffile = True
-				
-				Exp_Sn=seqNo(Exp_Sn+1)
-				
-				ack_pkt={
-				'sq_no': Exp_Sn
-				}
-				# Convert The Packet Into Byte Format
-				byte_pkt=pickle.dumps(ack_pkt)
+				if(rcv_pkt['sq_no']==Exp_Sn):
+					if_dropped=0
+					
+					if first_frame_check:
+						first_frame_time=time.time()
+						first_frame_check=False
+					
+					
+					print ("Received Packet inorder", Exp_Sn)
+					if rcv_pkt['data']:
+						f.write(rcv_pkt['data'])
+					else:
+						endoffile = True
+					
+					Exp_Sn=seqNo(Exp_Sn+1)
+					
+					ack_pkt={
+					'sq_no': Exp_Sn
+					}
 
-				# Create Checksum 
-				check_sum=hashlib.sha224(byte_pkt).hexdigest()
+					# Convert The Packet Into Byte Format
+					byte_pkt=pickle.dumps(ack_pkt)
 
-				# Add Checksum to Pakect
-				ack_pkt['check_sum']=check_sum
-				# Send Packet
-				conn.sendall(pickle.dumps(ack_pkt))
-				print('New Ack', repr(Exp_Sn))
+					# Create Checksum 
+					check_sum=hashlib.sha224(byte_pkt).hexdigest()
+
+					# Add Checksum to Pakect
+					ack_pkt['check_sum']=check_sum
+
+					# Send Packet
+					conn.sendall(pickle.dumps(ack_pkt))
+					print('New Ack Sent For', repr(Exp_Sn))
+		else:
+			if_dropped=1
+			print ("Packet Dropped", rcv_pkt['sq_no'])
+					
+			ack_pkt={
+			'sq_no': Exp_Sn
+			}
+			# Convert The Packet Into Byte Format
+			byte_pkt=pickle.dumps(ack_pkt)
+
+			# Create Checksum 
+			check_sum=hashlib.sha224(byte_pkt).hexdigest()
+
+			# Add Checksum to Pakect
+			ack_pkt['check_sum']=check_sum
+
+			# Send Packet
+			conn.sendall(pickle.dumps(ack_pkt))
+			print('Ack Sent For Same Packet', repr(Exp_Sn))
 
 	except Exception as e:
-		if endoffile:
-			if(time.time()-lastpktreceived>1):
-				break
-	
+		break
 
 endtime = time.time()
 
 f.close()
-print ('FILE TRANFER SUCCESSFUL')
-print ("TIME TAKEN " , str(endtime - starttime))
-
 conn.close()
+
+print ('FILE TRANFER SUCCESSFUL')
 print("Server Disconnected")
+
+print("--------------------------")
+
+print("Start Time", starttime)
+print("First Frame Received at",first_frame_time)
+print("End Time", endtime)
+
+print("--------------------------")
+
 
